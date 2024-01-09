bde547a9cd6af64ba2696ab09513644f212e0413
lmromero18 <lmromero.18@est.ucab.edu.ve>
2024-01-09 12:10:44 -0400

    Warehouse Pruebas unitarias completadas

--- backend/tests/warehouse_tests/test_warehouse.py
+++ backend/tests/warehouse_tests/test_warehouse.py
@@ -40,12 +40,45 @@ class TestWarehouseGetRoutes:
         res = await client.get(app.url_path_for("warehouse:get-warehouse-by-id", id="123e4567-e89b-12d3-a456-426614174000"))
         assert res.status_code != status.HTTP_404_NOT_FOUND
 
-#este hay que revisarlo
-#class TestWarehouseGetById:
-    #async def test_warehouse_by_id_route(self, authorized_client: AsyncClient, app:FastAPI) -> None:
-        #client = await authorized_client
-        #res = await client.get(app.url_path_for("warehouse:get-warehouse-by-id", id="1"))
-        #assert res.status_code == status.HTTP_200_OK
+class TestWarehouseGetById:
+    async def test_warehouse_by_id_route(self, authorized_client: AsyncClient, app:FastAPI) -> None:
+        client = await authorized_client
+        warehouse = await client.post(app.url_path_for("warehouse:create-warehouse"), json={"warehouse": {"type": "test"}})
+        warehouse_list_response = await client.get(app.url_path_for("warehouse:warehouse_list"))
+        warehouse_list = warehouse_list_response.json().get("data")[0]
+        warehouse_id = warehouse_list.get("id")
+        
+        # Include the id in the URL, not the request body
+        res = await client.get(app.url_path_for("warehouse:get-warehouse-by-id", id=warehouse_id))
+        assert res.status_code == status.HTTP_200_OK
+        
+class TestWarehouseUpdateById:
+    async def test_warehouse_update_by_id_route(self, authorized_client: AsyncClient, app:FastAPI) -> None:
+        client = await authorized_client
+        warehouse = await client.post(app.url_path_for("warehouse:create-warehouse"), json={"warehouse": {"type": "test"}})
+        warehouse_list_response = await client.get(app.url_path_for("warehouse:warehouse_list"))
+        warehouse_list = warehouse_list_response.json().get("data")[0]
+        warehouse_id = warehouse_list.get("id")
+        
+        warehouse_update = {"type": "updated_test"} 
+        res = await client.put(app.url_path_for("warehouse:update-warehouse-by-id", id=warehouse_id), json={"warehouse_update": warehouse_update})
+        assert res.status_code == status.HTTP_200_OK
+
+class TestWarehouseDeleteById:
+    async def test_warehouse_delete_by_id_route(self, authorized_client: AsyncClient, app:FastAPI) -> None:
+        client = await authorized_client
+        # Primero, creamos un almacén
+        warehouse = await client.post(app.url_path_for("warehouse:create-warehouse"), json={"warehouse": {"type": "test"}})
+        warehouse_list_response = await client.get(app.url_path_for("warehouse:warehouse_list"))
+        warehouse_list = warehouse_list_response.json().get("data")[0]
+        warehouse_id = warehouse_list.get("id")
+        
+        # Luego, eliminamos el almacén
+        res = await client.delete(app.url_path_for("warehouse:delete-warehouse-by-id", id=warehouse_id))
+        assert res.status_code == status.HTTP_200_OK
+
+
+
 
 
       
